0e206c366349dc37734b84ba0ef2d70b8e85697e
Amya Humsa <amya.humsa@gmail.com>
2025-11-14 19:13:07 +0100

    3

--- python_network/server.py
+++ python_network/server.py
@@ -2,4 +2,15 @@ import socket
 import threading
 
 host='127.0.0.1'
+port = 77777
 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients=[]
+nicknames=[]
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
